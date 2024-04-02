5fd1532ace079848a51a99c365d78eda36f15941
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-02 12:20:09 +0200

    compilato le migration i seeder e i model e popolato il database

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -15,7 +15,8 @@ class Project extends Model
         'title',
         'content',
         'slug',
-        'cover_image'
+        'cover_image',
+        'type_id'
     ];
 
     // protected $table = "projects";
@@ -23,5 +24,10 @@ class Project extends Model
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
