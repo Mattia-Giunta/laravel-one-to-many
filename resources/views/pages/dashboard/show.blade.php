4e59a6f9ca2dd83b068c115974c3c1509c37c492
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-02 12:57:58 +0200

    modificato la show.blade

--- resources/views/pages/dashboard/show.blade.php
+++ resources/views/pages/dashboard/show.blade.php
@@ -16,6 +16,12 @@
                         <img class="img-fluid" src="{{ asset('/storage/' . $project->cover_image) }}" alt="{{ $project->title }}">
                     @endif
 
+                    <p>
+                        <strong>
+                            {{ $project->type ? $project->type->name : 'Non è stato selezionato un Tipo'}}
+                        </strong>
+                    </p>
+
                     <p class="mt-3 text-uppercase ">{{ $project->content }}</p>
 
                 </div>
